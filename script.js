81c9d42ec752f2e3aad6368d70e032cd5f246347
Greg Schuman <gcschuman@hotmail.com>
2020-07-10 22:21:49 -0700

    Added the seat click event.

--- script.js
+++ script.js
@@ -52,6 +52,16 @@ movieSelect.addEventListener('change', e => {
     updateSelectedCount();
   });
 //Seat click event
+container.addEventListener('click', e => {
+    if (
+      e.target.classList.contains('seat') &&
+      !e.target.classList.contains('occupied')
+    ) {
+      e.target.classList.toggle('selected');
+  
+      updateSelectedCount();
+    }
+  });
 //Initial count and total set
 updateSelectedCount();
 
