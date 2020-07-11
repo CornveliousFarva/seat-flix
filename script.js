4b06fddc34f5986964d60fd63ab0f8e5bad4e485
Greg Schuman <gcschuman@hotmail.com>
2020-07-10 22:20:27 -0700

    Added get data from localStorage and populate UI.

--- script.js
+++ script.js
@@ -28,7 +28,25 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
   }
 //Get data from localStorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) > -1) {
+          seat.classList.add('selected');
+        }
+      });
+    }
+  
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  
+    if (selectedMovieIndex !== null) {
+      movieSelect.selectedIndex = selectedMovieIndex;
+    }
+  }
 //Movie select event
+
 //Seat click event
 //Initial count and total set
 updateSelectedCount();
