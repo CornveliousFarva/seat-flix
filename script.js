b0c67208700d8e24f1a951c7885e9526ea41f936
Greg Schuman <gcschuman@hotmail.com>
2020-07-10 22:02:40 -0700

    Added the function ot save movie and price.

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
